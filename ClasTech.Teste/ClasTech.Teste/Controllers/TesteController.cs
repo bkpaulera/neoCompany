f6c25529918e144d48ac9baea56e1bcd23a4cccd
Paulo <paulo_esilva@yahoo.com.br>
2023-11-22 11:36:38 -0300

    Project

---
+++ ClasTech.Teste/ClasTech.Teste/Controllers/TesteController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ClasTech.Teste.Models;
+using ClasTech.Teste.Service;
+
+namespace ClasTech.Teste.Controllers
+{
+    public class TesteController : ApiController
+    {
+        private readonly TesteService _testeService;
+
+        public TesteController()
+        {
+            _testeService = new TesteService();
+        }
+
+        [HttpPost]
+        public IList<TesteViewModel> GetAll()
+        {
+            return _testeService.GetAll();
+        }
+    }
+}
---
+++ ClasTech.Teste/ClasTech.Teste/Scripts/app/app.js
@@ -0,0 +1,28 @@
+﻿var app = angular.module('app', [
+    'ngRoute',
+    'ngCookies',
+    'ngAnimate',
+    'ngSanitize',
+    'pedido'
+]);
+
+app.config(['$provide', '$routeProvider', function ($provide, $routeProvider) {
+    $routeProvider.when('/pedido', {
+        templateUrl: '/Content/templates/pedido.html',
+        controller: 'pedidoCtrl'
+    });
+}]);
+
+//GLOBAL FUNCTIONS - pretty much a root/global controller.
+//Get username on each page
+//Get updated token on page change.
+//Logout available on each page.
+app.run(['$rootScope', '$http', '$cookies', '$cookieStore', '$location', function ($rootScope, $http, $cookies, $cookieStore, $location) {
+
+}]);
+
+$(document).ready(function () {
+    $('.table .valor').click(function () {
+        $(this).css('background-color', 'aquablue')
+    })
+})
