419a090f5d60f2ce7db248b30fc4147f1852c604
Paulo <paulo_esilva@yahoo.com.br>
2023-11-23 10:50:30 -0300

    Models

---
+++ ClasTech/ClasTech.Teste.Business.Entities/pedido.cs
@@ -0,0 +1,14 @@
+﻿namespace ClasTech.Teste.Business.Entities
+{
+    public class pedido
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal ValorTotal { get; set; }
+        public DateTime DataPedido { get; set; }
+        public bool Ativo { get; set; }
+        public List<pedidoItem> pedidoItem { get; set; }
+
+    }
+}
