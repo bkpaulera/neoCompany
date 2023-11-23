fc8a9b23eec81b0c23d9cc2fe8d16904dd8558a9
Paulo <paulo_esilva@yahoo.com.br>
2023-11-23 09:35:05 -0300

    Build Project
    Service With Testss

---
+++ ClasTech/ClasTech.Teste.Service/TesteService.cs
@@ -0,0 +1,25 @@
+﻿namespace ClasTech.Teste.Service
+{
+    public class TesteService
+    {
+
+        //- Consultar o pedido que tenha o maior valor total
+        public void GetOrderHighestValue() { }
+
+        // - Consultar pedidos item por nome
+        public string GetOrderByName(string name)
+        {
+            return name;
+        }
+
+        // - Consultar apenas os pedidos ativos
+        public void GetActiveOrders() { }
+
+        // - Consultar apenas pedidos realizados no ano de 2017
+        public void GetOrderByYear() { }
+
+        // - Consultar pedidos item(de todos os pedidos) onde o valor seja menor que R$ 18,50
+        public void GetItemOrderByValue() { }
+
+    }
+}
---
+++ ClasTech/ClasTech.Teste.Tests/Controllers/TesteController.cs
@@ -0,0 +1,45 @@
+using ClasTech.Teste.Service;
+
+namespace ClasTech.Teste.Tests.Controllers
+{
+    [TestClass]
+    public class TesteController
+    {
+        private TesteService _service;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _service = new TesteService();
+        }
+
+        [TestMethod]
+        public void Index() { }
+
+        [TestMethod]
+        //- Consultar o pedido que tenha o maior valor total
+        public void GetOrderHighestValue() { }
+
+        [TestMethod]
+        // - Consultar pedidos item por nome
+        public void GetOrderByName()
+        {
+            string order = "Vsalue";
+            var value = _service.GetOrderByName("Value");
+
+            Assert.IsTrue(order == "value");
+        }
+
+        [TestMethod]
+        // - Consultar apenas os pedidos ativos
+        public void GetActiveOrders() { }
+        [TestMethod]
+        // - Consultar apenas pedidos realizados no ano de 2017
+        public void GetOrderByYear() { }
+        [TestMethod]
+        // - Consultar pedidos item(de todos os pedidos) onde o valor seja menor que R$ 18,50
+        public void GetItemOrderByValue() { }
+
+
+    }
+}
